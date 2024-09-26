5fbee5867a4c1b7d039a34393e2f11f9407e34d6
Mahigavale <Maheshgavale07@gmail.com>
2024-09-26 17:51:04 +0530

    join

---
+++ Join.js
@@ -0,0 +1,9 @@
+
+
+let arr=[1,2,3,4,5,6,7,8];
+console.log(arr.join());
+
+//if we don't specify the joner for the given array then it by default joins with the help of the Comma.
+//it is just opposite of the join and does the exact opposite work.
+console.log(arr.join("**"));
+
