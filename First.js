334bfbd65d917df58234f614706641cecbce930f
Mahigavale <Maheshgavale07@gmail.com>
2024-09-26 11:26:58 +0530

    Find method in js

--- First.js
+++ First.js
@@ -1,3 +1,17 @@
 
 
 console.log("demo");
+
+const arr=[1,23,45,65,77,88];
+
+
+console.log(arr.find((number)=>
+{
+    if(number>40)
+        return number;
+        
+}));
+
+
+//the find method in js does return the single value but if we log then it will give us all the numbers 
+//that passes the specific test or condition designed for it.
