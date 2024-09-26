c1f6f230a0fc44094d0899ed1bd0ef0c970a736e
Mahigavale <Maheshgavale07@gmail.com>
2024-09-26 13:04:10 +0530

    to spliced in js

--- SpliceLogic.js
+++ SpliceLogic.js
@@ -5,3 +5,14 @@ let numbers=[1,3,5,7,9]
 
 numbers.splice(1,4,2,3,4,5,6,7,8);
 console.log(numbers);
+
+let fruits=['apple','banana','mango','potato'];
+
+
+let numbers2=fruits.toSpliced(2,0,'kel');
+console.log(numbers2);
+console.log(fruits);
+
+
+//toSpliced is a copying version of the Splice.
+//toSpliced does not modify the original array.
