88d43c76c728e9bb3a5690aea949ca24f45f1d1c
Mahigavale <Maheshgavale07@gmail.com>
2024-09-28 15:03:49 +0530

    keys and values in array

---
+++ keys.js
@@ -0,0 +1,21 @@
+
+
+let arr=[10,20,30,40,50,60];
+
+let obj=arr.keys();
+
+//prints the indices of the given array.
+
+for(i of obj)
+{
+    console.log(i);
+    
+}
+
+let obj1=arr.values();
+//prints the values of the given array.
+for(j of obj1)
+{
+    console.log(j);
+    
+}
