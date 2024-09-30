6b7dbb6c3ca4c93cbf8c59b21c5e0e72fc042cdb
Mahigavale <Maheshgavale07@gmail.com>
2024-09-30 13:12:28 +0530

    String methods

---
+++ String/S2.js
@@ -0,0 +1,11 @@
+
+
+let str=" I am the Mahesh Gavale !";
+
+console.log(str.charAt(6));
+//console.log(str.At(-3));
+console.log(str.charCodeAt(4));
+console.log(str[5]);
+
+
+
---
+++ With.js
@@ -0,0 +1,10 @@
+
+
+let arr1 = ['a', 'b', 'd'];
+
+
+let arr2 = arr1.with(2, 'c');
+
+console.log(arr2);
+
+//with is used to replace the missing or invalid character from the given array.
