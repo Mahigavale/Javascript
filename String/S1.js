67956de7f16358c627d1f4899818ca317b8641f8
Mahigavale <Maheshgavale07@gmail.com>
2024-09-30 12:59:48 +0530

    string

---
+++ String/S1.js
@@ -0,0 +1,10 @@
+
+//1) String Literal
+
+const str='i am the best';
+const str2="i am the best";
+const str3=`i am the best`;  //template Literal.
+
+const str4=new String('i am the  best !');
+
+console.log(str,str2,str3,str4);
