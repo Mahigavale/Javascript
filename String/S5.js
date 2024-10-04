5b64849bbf3cc9dfa6e92d86bf131be8be348ec5
Mahigavale <Maheshgavale07@gmail.com>
2024-10-04 14:37:54 +0530

    repeat in js

---
+++ String/S5.js
@@ -0,0 +1,8 @@
+
+
+let movie_name="mahesh ashok gavale ";
+console.log(movie_name.repeat(0));
+console.log(movie_name.repeat(5));
+
+//returns the copies of the given string 
+//if the count is zero then returns the empty string.
