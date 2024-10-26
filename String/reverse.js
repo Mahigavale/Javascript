e8aa7ca96504f8e141a48787886d2ebda2b238e6
Mahigavale <Maheshgavale07@gmail.com>
2024-10-26 15:36:28 +0530

    p99soft

---
+++ String/reverse.js
@@ -0,0 +1,15 @@
+
+
+let names="mahesh ashok gavale";
+
+let arr=names.split("");
+
+let arr2=arr.reverse();
+
+let str2=arr2.join("");
+console.log(arr);
+console.log(str2);
+
+//use split("") converts to array
+// array.reverse => reverses the array.
+//array.join("")=> joins the array to string.
