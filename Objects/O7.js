183b99835b93d1212855681b7ac6798640433c81
Mahigavale <Maheshgavale07@gmail.com>
2024-10-26 15:40:42 +0530

    p99soft

---
+++ Objects/O7.js
@@ -0,0 +1,14 @@
+
+
+let names=
+{
+    name:"mahesh",
+    email:"gavale07@gmail.com",
+    mobile:"1234567890"
+}
+
+for(const[k,v] of Object.entries(names))
+{
+    console.log(`${k}:${v}`);
+    
+}
