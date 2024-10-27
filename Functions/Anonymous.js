f350b9815c1c30b1448922095cace4e9f7b75b2d
Mahigavale <Maheshgavale07@gmail.com>
2024-10-27 14:37:11 +0530

    anonymous:1)normal 2)arrow 3)IIFE

---
+++ Functions/Anonymous.js
@@ -0,0 +1,63 @@
+//no function name.
+
+demo();
+//hoisting 
+//1) assigning to variable.
+const names= function()
+{
+    return "mahesh";
+}
+console.log(names());
+
+//passing as an argument
+
+let numbers=[10,20,30,40,50];
+//map,filter,foreach
+
+
+
+
+console.log(numbers.map((n)=>
+{
+    return n*2;
+}));
+
+
+console.log(numbers.filter((n)=>
+{
+    if(n>20)
+    {
+        return n;
+    }
+}));
+
+numbers.forEach((n)=>
+{
+    console.log(n*2);
+    
+})
+
+console.log("normal anonymous ");
+
+
+console.log(numbers.map(function(n)
+{
+    return n *2;
+}))
+
+
+
+function demo()
+{
+    console.log("hello wolrd !");
+    
+}
+
+
+
+
+(function()
+{
+    console.log("immediate");
+    
+})();
