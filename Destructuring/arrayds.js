9fb0d207c26f2318465697122510dbb79253b963
Mahigavale <Maheshgavale07@gmail.com>
2024-10-28 17:48:17 +0530

    destructring

--- Destructuring/arrayds.js
+++ Destructuring/arrayds.js
@@ -23,3 +23,28 @@
 
 //iffe can be named as well as anonymous.
 //it can be parameterized as well as non-parameterized.
+
+
+
+let names=['mahesh','ashok','gavale'];
+
+let[a,...s]=names;
+console.log(a);
+console.log(s);
+
+
+let names2={
+    name:"mahesh",
+    email:"mgavale07@gmail.com",
+    phone:"7774834341",
+    city:"Pune",
+    state:"maharashtra"
+}
+
+
+let {email,...rest}=names2;
+
+console.log(email);
+console.log(rest);
+
+
